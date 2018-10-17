0a95cd3af77ceb2150101b29d5167be23e0644d7
Bruno Henrique <brunohs.dev@gmail.com>
2018-10-17 08:49:25 -0300

    starting project

---
+++ src/server/server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const router = require('../router/router.js');
+
